67958419876266c8250e5af66d26ee1ea59560ac
DainP <32358700+DainP@users.noreply.github.com>
2017-09-28 12:51:18 +0100

    Added Two More LEDs  that blink

--- lab_101/2_blinky_with_shu_libraries/src/main.c
+++ lab_101/2_blinky_with_shu_libraries/src/main.c
@@ -19,7 +19,9 @@
 #include "gpio.h"
 
 // map the led to GPIO PI1 (again, this is the inbuilt led)
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led2 = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t led3 = {PB_15, GPIOB, GPIO_PIN_15};
 
 // this is the main method
 int main()
@@ -30,15 +32,38 @@ int main()
   init_sysclk_216MHz();
   
   // initialise the gpio pins
-  init_gpio(led, OUTPUT);
+  init_gpio(led1, OUTPUT);
+	init_gpio(led2, OUTPUT);
+	init_gpio(led3, OUTPUT);
   
   // loop forever ...
   while(1)
   {
-    // toggle the led on the gpio pin
-    toggle_gpio(led);
+    // toggle the led on the gpio pin1
+    toggle_gpio(led1);
     
-    // wait for 1 second
-    HAL_Delay(1000);
+    // wait for 10 second
+    HAL_Delay(10000);
+		
+		 // toggle the led on the gpio pin1
+    toggle_gpio(led1);
+		
+		// toggle the led on the gpio pin14
+		toggle_gpio(led2);
+		
+		// wait for 1 second
+		 HAL_Delay(2000);
+		
+		// toggle the led on the gpio pin14
+		toggle_gpio(led2);
+		
+		// toggle the led on the gpio pin15
+		toggle_gpio(led3);
+		
+		// wait for 1 second
+		 HAL_Delay(10000);
+		 
+		 // toggle the led on the gpio pin15
+		toggle_gpio(led3);
   }
 }
