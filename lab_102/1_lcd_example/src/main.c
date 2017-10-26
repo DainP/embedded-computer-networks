811d110e4f87be9991a7e4783221a028f55743aa
DainP <32358700+DainP@users.noreply.github.com>
2017-10-26 12:20:08 +0100

    Controled Leds with pot and made bar graph
    Different LEDs light up depending on the value of the adc, A bar chart is then created, changing its length depending on the adc value

--- lab_102/1_lcd_example/src/main.c
+++ lab_102/1_lcd_example/src/main.c
@@ -18,8 +18,14 @@
 // Include the studio libary
 #include <stdio.h>
 #include "adc.h"
+#include "gpio.h"
 
 gpio_pin_t pot = {PA_0, GPIOA, GPIO_PIN_0};
+
+// Map Leds to pins
+gpio_pin_t led1 = {PF_10,GPIOF,GPIO_PIN_10};
+gpio_pin_t led2 = {PF_9,GPIOF,GPIO_PIN_9};
+gpio_pin_t led3 = {PF_8,GPIOF,GPIO_PIN_8};
 
 // LCD DEFINES
 
@@ -42,8 +48,12 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
-  
+  float bar;
 	init_adc(pot);
+	init_gpio(led1,OUTPUT);
+	init_gpio(led2,OUTPUT);
+	init_gpio(led3,OUTPUT);
+	
   // initialise the lcd
   BSP_LCD_Init();
   BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, SDRAM_DEVICE_ADDR);
@@ -62,7 +72,8 @@ int main()
   BSP_LCD_DisplayStringAtLine(1, (uint8_t *)welcome_message[0]);
   BSP_LCD_DisplayStringAtLine(2, (uint8_t *)welcome_message[1]);
   BSP_LCD_DisplayStringAtLine(3, (uint8_t *)BOARDER); 
-    
+  
+  
   // delay a little ...
   HAL_Delay(5000);
   
@@ -78,12 +89,45 @@ int main()
 //    // print the message to the lcd
 //    BSP_LCD_ClearStringLine(6);
 //    BSP_LCD_DisplayStringAtLine(6, (uint8_t *)str);
+		
 		uint16_t adc_val = read_adc(pot);
 		float percent = (adc_val/4095.0) * 100;
+		
+		
+		//Drawing a rectangle - BSP_LCD_FillRect(xpos,ypos,  Height,  Width)    
+		bar = percent * 4.8;
+		BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+		BSP_LCD_FillRect(0,200,bar,20);
+		
 		char str[20];
 		sprintf(str, "ADC = %3.2f", percent);
 		BSP_LCD_DisplayStringAtLine(6, (uint8_t *)str);
-    
+		
+		
+		if (adc_val > 0 && adc_val< 1365)
+		{
+			write_gpio(led1, HIGH);	
+			write_gpio(led2, LOW);
+			write_gpio(led3, LOW);
+		}
+		
+		if (adc_val > 1364 && adc_val < 2730)
+		{
+			write_gpio(led1, LOW);	
+			write_gpio(led2, HIGH);
+			write_gpio(led3, LOW);
+		}
+		
+		if (adc_val > 2729)
+		{
+			write_gpio(led1, LOW);	
+			write_gpio(led2, LOW);
+			write_gpio(led3, HIGH);
+		}
+		
+	
     HAL_Delay(1000);
+		BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
+		BSP_LCD_FillRect(0,200,480,20);
   }
 }
